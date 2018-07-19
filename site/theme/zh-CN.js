16ba71dde8f4bac7d3be77ab93b472cab25c566c
王楠 <wangnan16@jd.com>
2018-07-19 17:21:03 +0800

    test

---
+++ site/theme/zh-CN.js
@@ -0,0 +1,14 @@
+const appLocaleData = require('react-intl/locale-data/zh');
+
+module.exports = {
+  locale: 'zh-CN',
+  data: appLocaleData,
+  messages: {
+    'app.header.menu.components': '组件',
+    'app.header.lang': 'English',
+    'app.component.examples': '代码演示',
+    'app.demo.copy': '复制代码',
+    'app.demo.copied': '复制成功',
+    'app.footer.company': 'IBD FE',
+  },
+};
