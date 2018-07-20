ab3c9e83ac7057aaf8fb4eddd0a27106787cad45
王楠 <wangnan16@jd.com>
2018-07-20 18:58:25 +0800

    头部选择逻辑

--- src/datepicker/index.jsx
+++ src/datepicker/index.jsx
@@ -87,9 +87,8 @@ export default class Datepicker extends React.Component {
         const value = e.target.dataset.time;
         const { onChange } = this.props;
         this.setState({
-            selectedValue: value
-        });
-        this.setState({
+            selectedValue: value,
+            loopValue: value,
             daysMap: this.getDays(new Date(value))
         });
         onChange && onChange(value);
@@ -117,10 +116,24 @@ export default class Datepicker extends React.Component {
         }
         return cls;
     }
-    lastYear = year => {
+    lastYear = (year, month) => {
+        console.log(typeof year)
         this.setState({
-            daysMap: this.getDays(new Date(`${year}-01-01`))
+            daysMap: this.getDays(new Date(`${Number(year,10) - 1}-${month}-01`)),
+            loopValue: `${year - 1}-${month}-01`
         });
+    }
+    lastMonth = (year, month) => {
+        if(month === '01'){
+            year = Number(year) - 1;
+            month = '12';
+        } else {
+            month = (Number(month) - 1) < 10 ? `0${Number(month) - 1}` : `${Number(month) - 1}`;
+        }
+        this.setState({
+            daysMap: this.getDays(new Date(`${year}-${month}-01`)),
+            loopValue: `${year}-${month}-01`
+        })
     }
     yearChange = () => {
 
@@ -132,19 +145,19 @@ export default class Datepicker extends React.Component {
         const {
             nowValue, selectedValue, loopValue, openModal, weekText, daysMap
         } = this.state;
-        const value = selectedValue || nowValue;
+        const value = loopValue || nowValue;
         const navYear = value && `${value.split('-')[0]}年`;
         const navMonth = value && `${value.split('-')[1]}月`;
         return (
             <div className="react-datepicker" onMouseEnter={this.inputEnter} onMouseLeave={this.inputLeave}>
                 <div className="datepicker-input">
-                    <input readOnly type="text" className="c-input" value={value} placeholder="请选择日期" />
+                    <input readOnly type="text" className="c-input" value={selectedValue || nowValue} placeholder="请选择日期" />
                     <span className="datepicker-icon"></span>
                 </div>
                 <div className="datepicker-modal" style={{ display: openModal ? 'block' : 'none' }}>
                     <div className="datepicker-modal-top">
-                        <span className="top-year-left" title="上一年" onClick={() => this.lastYear(value.split('-')[0])}></span>
-                        <span className="top-month-left" title="上个月" onClick={() => this.lastMonth(value.split('-')[1])}></span>
+                        <span className="top-year-left" title="上一年" onClick={() => this.lastYear(value.split('-')[0],value.split('-')[1])}></span>
+                        <span className="top-month-left" title="上个月" onClick={() => this.lastMonth(value.split('-')[0],value.split('-')[1])}></span>
                         <span className="top-content">
                             <span className="year-change" onClick={this.yearChange}>{navYear}</span>
                             <span className="month-change" onClick={this.monthChange}>{navMonth}</span>
