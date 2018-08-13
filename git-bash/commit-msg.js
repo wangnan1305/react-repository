4222f1011ae19ec3e44c75b19ababe74289679c1
王楠 <wangnan16@jd.com>
2018-08-13 16:52:45 +0800

    test

--- git-bash/commit-msg.js
+++ git-bash/commit-msg.js
@@ -2,13 +2,13 @@ const fs = require('fs');
 const runner = require('unity-git-commit').runner;
 try {
     console.log(__dirname);
-    let message = fs.readFileSync('../.git/COMMIT_EDITMSG', 'utf-8');
-    const lines = message.split('\n');
-    if (!lines[lines.length - 1]) {
-        lines.pop();
-    }
-    message = lines.join('\n');
-    runner(message);
+    let message = fs.readFileSync('../.git', 'utf-8');
+    // const lines = message.split('\n');
+    // if (!lines[lines.length - 1]) {
+    //     lines.pop();
+    // }
+    // message = lines.join('\n');
+    // runner(message);
 } catch (e) {
     console.log('检测程序运行出错...', e);
 }
