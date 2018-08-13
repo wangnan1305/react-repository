94b35d6134de9282b5edc3c59413c88e685e5213
王楠 <wangnan16@jd.com>
2018-08-13 19:14:39 +0800

    【全局模块】使用unity-git-commit

--- git-bash/commit-msg.js
+++ git-bash/commit-msg.js
@@ -1,14 +1,13 @@
 const fs = require('fs');
 const runner = require('unity-git-commit').runner;
 try {
-    console.log(__dirname);
-    let message = fs.readFileSync('./.git/COMMIT_EDITMSG', 'utf-8');
-    // const lines = message.split('\n');
-    // if (!lines[lines.length - 1]) {
-    //     lines.pop();
-    // }
-    // message = lines.join('\n');
-    // runner(message);
+    let message = fs.readFileSync('/Users/wangnan16/Desktop/react-datepicker/.git/COMMIT_EDITMSG', 'utf-8');
+    const lines = message.split('\n');
+    if (!lines[lines.length - 1]) {
+        lines.pop();
+    }
+    message = lines.join('\n');
+    runner(message);
 } catch (e) {
     console.log('检测程序运行出错...', e);
 }
